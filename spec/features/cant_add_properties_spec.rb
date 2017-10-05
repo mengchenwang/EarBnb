f9710fc3dcadaaf0ba498360d5a5714e302b9380
Nero Siva <neroshan12@gmail.com>
2017-10-05 17:40:20 +0100

    Add test so user cannot make a listing without signing in

---
+++ spec/features/cant_add_properties_spec.rb
@@ -0,0 +1,7 @@
+feature 'Creating properties' do
+
+  scenario 'I cant create a new peoperty without signing in' do
+    visit '/properties/new'
+    expect(current_path).to eq('/properties')
+  end
+end
